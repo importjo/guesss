7f71458c7da10f124cff0d8dc5429a193fa0cd06
Jonathan Battisti <jonathan.battisti.1@gmail.com>
2019-08-24 15:13:00 -0400

    Change files structure

--- guesss_test.go
+++ guesss_test.go
@@ -1,4 +1,4 @@
-package main
+package guesss
 
 import (
 	"testing"
@@ -6,12 +6,15 @@ import (
 	"github.com/stretchr/testify/assert"
 )
 
+var guess int
+var answer int
+
 func TestIsGuessCorrect(t *testing.T) {
 	guess = 4500
 	answer = 4500
-	assert.Equal(t, "You found the answer.", isGuessCorrect(guess, answer))
+	assert.Equal(t, "You found the answer.", IsGuessCorrect(guess, answer))
 	guess = 4000
-	assert.Equal(t, "Wrong answer. Your guess is lower.", isGuessCorrect(guess, answer))
+	assert.Equal(t, "Wrong answer. Your guess is lower.", IsGuessCorrect(guess, answer))
 	guess = 4700
-	assert.Equal(t, "Wrong answer. Your guess is higher.", isGuessCorrect(guess, answer))
+	assert.Equal(t, "Wrong answer. Your guess is higher.", IsGuessCorrect(guess, answer))
 }
